b1b125cbbc345c1681627ef6db6aa96297d674e9
AustinLinxm <xiaoming.lin@stonybrook.edu>
2017-01-04 22:06:33 -0500

    finish with the camera follow

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -15,6 +15,17 @@ public class CameraFollow : MonoBehaviour {
 
     void FixedUpdate()
     {
+        if (Input.GetKeyDown("d"))
+        {
+            if (target.rotation != Quaternion.Euler(0, 90f, 0))
+                offset.x = (transform.position - target.position).x + (12.17f * 2);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            if (target.rotation != Quaternion.Euler(0, -90f, 0))
+                offset.x = (transform.position - target.position).x - (12.17f * 2);
+        }
+
         Vector3 targetCamPos = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
